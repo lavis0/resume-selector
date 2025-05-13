4a667eb49aa294a8a73ed5676c7adeb86420529d
Joshua Banga <joshuabanga@hotmail.com>
2025-05-13 16:54:44 +0100

    basic skill functionality

--- include/Resume/Skill.h
+++ include/Resume/Skill.h
@@ -5,12 +5,75 @@
 #ifndef SKILL_H
 #define SKILL_H
 
+#include <string>
 
+/* TODO:
+ * - testing
+ */
 
-class Skill {
+namespace ResumeEditor {
 
+enum class SkillLevel {
+    Beginner,
+    Intermediate,
+    Advanced,
+    Expert
 };
 
+class Skill {
+public:
+    Skill() = default;
+    Skill(const std::string& name, SkillLevel level = SkillLevel::Intermediate,
+          bool selected = false) {
+        setName(name);
+        setLevel(level);
+        setSelected(selected);
+    }
+
+    // Getters and setters
+    const std::string& getName() const {
+        return name_;
+    }
+    void setName(const std::string& name) {
+        this->name_ = name;
+    }
+
+    SkillLevel getLevel() const {
+        return level_;
+    }
+    void setLevel(const SkillLevel level) {
+        this->level_ = level;
+    }
+
+    bool isSelected() const {
+        return selected_;
+    }
+    void setSelected(const bool selected) {
+        this->selected_ = selected;
+    }
+
+    // Utility methods
+    std::string getLevelAsString() const {
+        switch (level_) {
+            case SkillLevel::Beginner:
+                return "Beginner";
+            case SkillLevel::Intermediate:
+                return "Intermediate";
+            case SkillLevel::Advanced:
+                return "Advanced";
+            case SkillLevel::Expert:
+                return "Expert";
+        }
+        return nullptr; // TODO: exception
+    }
+
+private:
+    std::string name_;
+    SkillLevel level_;
+    bool selected_;
+};
+
+} // namespace ResumeEditor
 
 
 #endif //SKILL_H
